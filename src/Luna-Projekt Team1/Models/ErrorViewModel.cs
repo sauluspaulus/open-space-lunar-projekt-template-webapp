0c6fe3d734f0da6253307fd27b9b2f16aded739a
sauluspaulus <43391965+sauluspaulus@users.noreply.github.com>
2022-11-03 10:28:22 +0100

    Add files via upload

---
+++ src/Luna-Projekt Team1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Luna_Projekt_Team1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
